203f5148903025e55f14e2213ba166f3ccc61cef
Madeleine Woodbury <mail@woodburycode.com>
2023-04-18 14:16:51 -0400

    setup flask server

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
-import cloudinary
+from flask import Flask, jsonify
+from config import cloudinary, SECRET
 
-from dotenv import load_dotenv
-import os
+app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET
 
-load_dotenv()
+@app.route('/')
+def index():
+    try:
+        data = cloudinary.uploader.upload(file='images/dog.jpeg', 
+                                          public_id='dog_test_3',
+                                          folder='test')
+        
+        res = jsonify(data)
+        res.status_code = 200
+        return res
+        
+    except Exception as err:
+        print(type(err), err)
 
-cloudinary.config(
-    cloud_name=os.getenv('CLOUD_NAME'),
-    api_key=os.getenv('API_KEY'),
-    api_secret=os.getenv('API_SECRET'),
-    secure=True
-)
-
-import cloudinary.uploader
-import cloudinary.api
-
-results = cloudinary.uploader.upload(file='images/dog.jpeg', 
-                                     public_id='dog_test_2',
-                                     folder='test')
-
-for key, val in results.items():
-    print(f'{key}: {val}')
---
+++ config.py
@@ -0,0 +1,18 @@
+import cloudinary
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+cloudinary.config(
+    cloud_name=os.getenv('CLOUD_NAME'),
+    api_key=os.getenv('API_KEY'),
+    api_secret=os.getenv('API_SECRET'),
+    secure=True
+)
+
+import cloudinary.uploader
+import cloudinary.api
+
+SECRET = os.getenv('SECRET')
