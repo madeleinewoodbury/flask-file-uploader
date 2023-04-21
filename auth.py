fe24b3b37221f9628462274463b082947910fcd6
Madeleine Woodbury <mail@woodburycode.com>
2023-04-21 17:47:42 -0400

    logout user

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, render_template, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, current_user
+from flask_login import login_user, logout_user, login_required
 from forms import RegisterForm, LoginForm
 from models import User
 from app import db
@@ -63,3 +63,8 @@ def login():
 
     return render_template('login.html', form=form)
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
